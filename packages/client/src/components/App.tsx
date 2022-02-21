c557aaff3f9c050bd03f21dbdfb5447bf4d93940
victorSauceda <victorasauceda@gmail.com>
2022-02-21 11:37:25 -0800

    adding styling to center

--- packages/client/src/components/App.tsx
+++ packages/client/src/components/App.tsx
@@ -3,7 +3,13 @@ import Board from "./Board";
 
 export const App = () => (
   <main>
-    <div>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        paddingTop: "20rem",
+      }}
+    >
       <Board />
     </div>
   </main>
